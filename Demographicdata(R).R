54335a07b859572b4d0b6e03e964b2031745041c
Abhishekmathur-github <35617949+Abhishekmathur-github@users.noreply.github.com>
2018-01-23 01:43:51 -0600

    Update Demographicdata(R).R

--- Demographicdata(R).R
+++ Demographicdata(R).R
@@ -295,3 +295,77 @@ qplot(data=stats, x=Income.Group,y=Internet.users , size=I(01),color=I("Blue"),g
 qplot(data=stats,x=Income.Group,y=Internet.users, geom="boxplot")
 # scatter plot of Internet users and birthrate based on income group
 qplot(data=stats, x= Internet.users ,y=Birth.rate , color=Income.Group , size=I(3))
+
+#creating Data Frame
+#Execute below code to generate three new vectors
+Countries_2012_Dataset <- c("Aruba","Afghanistan","Angola","Albania","United Arab Emirates","Argentina","Armenia","Antigua and Barbuda","Australia","Austria","Azerbaijan","Burundi","Belgium","Benin","Burkina Faso","Bangladesh","Bulgaria","Bahrain","Bahamas, The","Bosnia and Herzegovina","Belarus","Belize","Bermuda","Bolivia","Brazil","Barbados","Brunei Darussalam","Bhutan","Botswana","Central African Republic","Canada","Switzerland","Chile","China","Cote d'Ivoire","Cameroon","Congo, Rep.","Colombia","Comoros","Cabo Verde","Costa Rica","Cuba","Cayman Islands","Cyprus","Czech Republic","Germany","Djibouti","Denmark","Dominican Republic","Algeria","Ecuador","Egypt, Arab Rep.","Eritrea","Spain","Estonia","Ethiopia","Finland","Fiji","France","Micronesia, Fed. Sts.","Gabon","United Kingdom","Georgia","Ghana","Guinea","Gambia, The","Guinea-Bissau","Equatorial Guinea","Greece","Grenada","Greenland","Guatemala","Guam","Guyana","Hong Kong SAR, China","Honduras","Croatia","Haiti","Hungary","Indonesia","India","Ireland","Iran, Islamic Rep.","Iraq","Iceland","Israel","Italy","Jamaica","Jordan","Japan","Kazakhstan","Kenya","Kyrgyz Republic","Cambodia","Kiribati","Korea, Rep.","Kuwait","Lao PDR","Lebanon","Liberia","Libya","St. Lucia","Liechtenstein","Sri Lanka","Lesotho","Lithuania","Luxembourg","Latvia","Macao SAR, China","Morocco","Moldova","Madagascar","Maldives","Mexico","Macedonia, FYR","Mali","Malta","Myanmar","Montenegro","Mongolia","Mozambique","Mauritania","Mauritius","Malawi","Malaysia","Namibia","New Caledonia","Niger","Nigeria","Nicaragua","Netherlands","Norway","Nepal","New Zealand","Oman","Pakistan","Panama","Peru","Philippines","Papua New Guinea","Poland","Puerto Rico","Portugal","Paraguay","French Polynesia","Qatar","Romania","Russian Federation","Rwanda","Saudi Arabia","Sudan","Senegal","Singapore","Solomon Islands","Sierra Leone","El Salvador","Somalia","Serbia","South Sudan","Sao Tome and Principe","Suriname","Slovak Republic","Slovenia","Sweden","Swaziland","Seychelles","Syrian Arab Republic","Chad","Togo","Thailand","Tajikistan","Turkmenistan","Timor-Leste","Tonga","Trinidad and Tobago","Tunisia","Turkey","Tanzania","Uganda","Ukraine","Uruguay","United States","Uzbekistan","St. Vincent and the Grenadines","Venezuela, RB","Virgin Islands (U.S.)","Vietnam","Vanuatu","West Bank and Gaza","Samoa","Yemen, Rep.","South Africa","Congo, Dem. Rep.","Zambia","Zimbabwe")
+Codes_2012_Dataset <- c("ABW","AFG","AGO","ALB","ARE","ARG","ARM","ATG","AUS","AUT","AZE","BDI","BEL","BEN","BFA","BGD","BGR","BHR","BHS","BIH","BLR","BLZ","BMU","BOL","BRA","BRB","BRN","BTN","BWA","CAF","CAN","CHE","CHL","CHN","CIV","CMR","COG","COL","COM","CPV","CRI","CUB","CYM","CYP","CZE","DEU","DJI","DNK","DOM","DZA","ECU","EGY","ERI","ESP","EST","ETH","FIN","FJI","FRA","FSM","GAB","GBR","GEO","GHA","GIN","GMB","GNB","GNQ","GRC","GRD","GRL","GTM","GUM","GUY","HKG","HND","HRV","HTI","HUN","IDN","IND","IRL","IRN","IRQ","ISL","ISR","ITA","JAM","JOR","JPN","KAZ","KEN","KGZ","KHM","KIR","KOR","KWT","LAO","LBN","LBR","LBY","LCA","LIE","LKA","LSO","LTU","LUX","LVA","MAC","MAR","MDA","MDG","MDV","MEX","MKD","MLI","MLT","MMR","MNE","MNG","MOZ","MRT","MUS","MWI","MYS","NAM","NCL","NER","NGA","NIC","NLD","NOR","NPL","NZL","OMN","PAK","PAN","PER","PHL","PNG","POL","PRI","PRT","PRY","PYF","QAT","ROU","RUS","RWA","SAU","SDN","SEN","SGP","SLB","SLE","SLV","SOM","SRB","SSD","STP","SUR","SVK","SVN","SWE","SWZ","SYC","SYR","TCD","TGO","THA","TJK","TKM","TLS","TON","TTO","TUN","TUR","TZA","UGA","UKR","URY","USA","UZB","VCT","VEN","VIR","VNM","VUT","PSE","WSM","YEM","ZAF","COD","ZMB","ZWE")
+Regions_2012_Dataset <- c("The Americas","Asia","Africa","Europe","Middle East","The Americas","Asia","The Americas","Oceania","Europe","Asia","Africa","Europe","Africa","Africa","Asia","Europe","Middle East","The Americas","Europe","Europe","The Americas","The Americas","The Americas","The Americas","The Americas","Asia","Asia","Africa","Africa","The Americas","Europe","The Americas","Asia","Africa","Africa","Africa","The Americas","Africa","Africa","The Americas","The Americas","The Americas","Europe","Europe","Europe","Africa","Europe","The Americas","Africa","The Americas","Africa","Africa","Europe","Europe","Africa","Europe","Oceania","Europe","Oceania","Africa","Europe","Asia","Africa","Africa","Africa","Africa","Africa","Europe","The Americas","The Americas","The Americas","Oceania","The Americas","Asia","The Americas","Europe","The Americas","Europe","Asia","Asia","Europe","Middle East","Middle East","Europe","Middle East","Europe","The Americas","Middle East","Asia","Asia","Africa","Asia","Asia","Oceania","Asia","Middle East","Asia","Middle East","Africa","Africa","The Americas","Europe","Asia","Africa","Europe","Europe","Europe","Asia","Africa","Europe","Africa","Asia","The Americas","Europe","Africa","Europe","Asia","Europe","Asia","Africa","Africa","Africa","Africa","Asia","Africa","Oceania","Africa","Africa","The Americas","Europe","Europe","Asia","Oceania","Middle East","Asia","The Americas","The Americas","Asia","Oceania","Europe","The Americas","Europe","The Americas","Oceania","Middle East","Europe","Europe","Africa","Middle East","Africa","Africa","Asia","Oceania","Africa","The Americas","Africa","Europe","Africa","Africa","The Americas","Europe","Europe","Europe","Africa","Africa","Middle East","Africa","Africa","Asia","Asia","Asia","Asia","Oceania","The Americas","Africa","Europe","Africa","Africa","Europe","The Americas","The Americas","Asia","The Americas","The Americas","The Americas","Asia","Oceania","Middle East","Oceania","Middle East","Africa","Africa","Africa","Africa")
+
+
+#(c) Kirill Eremenko, www.superdatascience.com
+
+NewDF<-data.frame(Countries_2012_Dataset,Codes_2012_Dataset,Regions_2012_Dataset)
+
+#Method1
+head(NewDF)
+colnames(NewDF)<-C("Country","Code","Region")
+colnames(NewDF)<-c("Country","Code","Region")
+head(NewDF)
+rm(NewDF)
+#Method2
+NewDF<-data.frame(Countries_2012_Dataset,Codes_2012_Dataset,Regions_2012_Dataset)
+Newdf<-data.frame(Country=Countries_2012_Dataset,Codes=Codes_2012_Dataset,Regions=Regions_2012_Dataset)
+Newdf
+head(Newdf)
+nrow(Newdf)#195
+ncol(Newdf)#3
+#summary of Newdf
+summary(Newdf)
+summary(Newdf$Country)
+summary(Newdf$Codes)
+summary(Newdf$Regions)
+#Summary of Stats df
+summary(stats)
+summary(stats$Internet.users)
+summary(stats$Income.Group)
+summary(stats$Country.Name)
+summary(stats$Country.Code)
+summary(stats$Birth.rate)
+
+#Merging data frames
+MergedDF<-merge(stats,Newdf,by.x = "Country.Code",by.y = "Codes")
+MergedDF
+summary(MergedDF)
+MergedDF$Country<-NULL
+head(MergedDF)
+tail(MergedDF)
+
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03))
+# Adding shapes to vizualization
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(01))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(02))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(03))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(04))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(05))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(06))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(07))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(08))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(09))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(10))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(11))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(12))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(13))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(14))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(15))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(16))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(17))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(18))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(19))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(20))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(21))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(22))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(23))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(24))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(25))
+qplot(data=MergedDF,x=Internet.users,y=Birth.rate , color= Regions , size=I(03), shape=I(26))#Does not exist
